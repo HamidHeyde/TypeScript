590e48138ff0d2e2a76310f3f652983354c2eda7
HamidHeyde <heidarzadeh.hamidreza@gmail.com>
2024-01-31 11:58:06 -0800

    updated the functions

--- functions/index.ts
+++ functions/index.ts
@@ -1,3 +1,4 @@
+// functions
 import { 
   addNumbers,
   concatStrings,
@@ -5,11 +6,9 @@ import {
   fetchData
 } from "./functions";
 
-console.log(
-  addNumbers(2,3), '\n',
-  concatStrings("hello"), '\n',
-  concatStrings("hello", "you"), '\n',
-  concatMultiStrings("hello"), '\n',
-  concatMultiStrings("hello", "to", "you"), '\n',
-  fetchData("www.google.com"), '\n'
-)
+console.log(addNumbers(2,3))
+console.log(concatStrings("hello"))
+console.log(concatStrings("hello", "you"))
+console.log(concatMultiStrings("hello"))
+console.log(concatMultiStrings("hello", "to", "you"))
+console.log(fetchData("www.google.com"))
