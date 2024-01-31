33f6e0e854beeaeaef76eb15d95ccc17cc69384d
HamidHeyde <heidarzadeh.hamidreza@gmail.com>
2024-01-30 17:25:30 -0800

    added main file for functions

--- functions/index.ts
+++ functions/index.ts
@@ -1,1 +1,13 @@
-console.log("hello")
+import { 
+  addNumbers,
+  concatStrings,
+  concatMultiStrings
+} from "./functions";
+
+console.log(
+  addNumbers(2,3), '\n',
+  concatStrings("hello"), '\n',
+  concatStrings("hello", "you"), '\n',
+  concatMultiStrings("hello"), '\n',
+  concatMultiStrings("hello", "to", "you"), '\n'
+)
