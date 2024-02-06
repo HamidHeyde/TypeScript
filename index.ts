91a6c357be05fd836bfd5821e0992ac1ee789663
HamidHeyde <heidarzadeh.hamidreza@gmail.com>
2024-02-05 18:40:51 -0800

    upadted for generics part2

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import "./0practices"
+// import "./0practices"
 // import './1basics'
 // import './2functions'
 // import "./3optionals"
 // import "./4tuples"
-// import "./5generics"
+import "./5generics"
