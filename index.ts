45b8e5e4c32c4c54d703e4779003c406679d2adf
HamidHeyde <heidarzadeh.hamidreza@gmail.com>
2024-02-05 16:45:22 -0800

    renamed directory

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-// import "./practices"
+import "./0practices"
 // import './1basics'
 // import './2functions'
 // import "./3optionals"
 // import "./4tuples"
-import "./5generics"
+// import "./5generics"
