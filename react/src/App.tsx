73a8ac1027ddfbecb71820119f7b48e7727c2758
HamidHeyde <heidarzadeh.hamidreza@gmail.com>
2024-02-14 18:25:53 -0800

    added user component to the APP

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
+import User, { tUser } from './components/user';
 
-function App() {
+const usersList: tUser[] = [
+  { id: 1, name: 'Leanne Graham', username: 'Bret' },
+  { id: 2, name: 'Ervin Howell', username: 'Antonette' },
+  { id: 3, name: 'Clementine Bauch', username: 'Samantha' },
+  { id: 4, name: 'Patricia Lebsack', username: 'Karianne' },
+  { id: 5, name: 'Chelsey Dirich', username: 'Kamren' }
+];
+
+const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Root>
+      <TextInput />
+      <User users={usersList} />
+    </Root>
   );
-}
+};
 
 export default App;
+
+const Root = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center'
+}));
+
+const TextInput = styled(TextField)(({ theme }) => ({
+  marginTop: '20px',
+  width: '300px'
+}));
