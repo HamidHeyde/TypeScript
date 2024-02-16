38a04732c7d2f3f6b582596074cba22b70d9137f
HamidHeyde <heidarzadeh.hamidreza@gmail.com>
2024-02-16 15:00:43 -0800

    removed  unneccessary files

--- portfolio/reactTypeScriptBasics/src/App.tsx
+++ portfolio/reactTypeScriptBasics/src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Parent from "./parentChild/Parent"
+import Child from "./parentChild/Child"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Parent/>
+    <Child color= "RED (being passed in)"/>
+  </div>
+)
 
-export default App;
+export default App
