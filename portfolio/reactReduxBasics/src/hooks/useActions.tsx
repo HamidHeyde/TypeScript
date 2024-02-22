d45d2a5ae12789f2e8a9e76997511398ef5d3c22
HamidHeyde <heidarzadeh.hamidreza@gmail.com>
2024-02-22 15:50:53 -0800

    renamed the file

--- portfolio/reactReduxBasics/src/hooks/useActions.tsx
+++
@@ -1,9 +0,0 @@
-import { useDispatch } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as actionCreators from '../redux/actionCreators';
-
-export const useActions = () => {
-  const dispatch = useDispatch();
-
-  return bindActionCreators(actionCreators, dispatch)
-};
