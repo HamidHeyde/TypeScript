59ebef92cb07a3d4f35f9c11cf90b00a7a4b7916
HamidHeyde <heidarzadeh.hamidreza@gmail.com>
2024-02-26 17:22:39 -0800

    updated app import

--- portfolio/reactReduxBasics/src/index.tsx
+++ portfolio/reactReduxBasics/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app';
+import App from './components/App';
 
 /**
  * Renders the root React component App wrapped in React.StrictMode into
