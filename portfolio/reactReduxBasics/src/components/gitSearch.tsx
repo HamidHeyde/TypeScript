cb1e0fd430b2b036235fd2a3f29997f84f9931a8
HamidHeyde <heidarzadeh.hamidreza@gmail.com>
2024-02-27 17:19:05 -0800

    updated code to consider new additions to traditional approach

--- portfolio/reactReduxBasics/src/components/gitSearch.tsx
+++ portfolio/reactReduxBasics/src/components/gitSearch.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useState } from 'react';
-import { useAppDispatch, useAppSelector } from '../newApproach/store';
-import { gitSearch } from '../newApproach/reducers/gitSearchReducers';
+// import { useAppDispatch, useAppSelector } from '../newApproach/store';
+// import { gitSearch } from '../newApproach/reducers/gitSearchReducers';
 // import useActionCreators from '../traditionalApproach/hooks/useActionCreators';
 // import useReduxStore from '../traditionalApproach/hooks/useReduxStore';
-
+import {
+  useAppDispatch,
+  useAppSelector
+} from '../traditionalApproach/redux/store';
+import { searchGitHubRepositories } from '../traditionalApproach/redux/actionCreators';
 /**
  * GitSearch component fetches Git repository search results.
  * Allows searching by query term and displays results.
@@ -18,6 +22,8 @@ const GitSearch: React.FC = () => {
     (state) => state.gitSearch
   );
 
+  useAppSelector((state) => console.log(state));
+
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(event.target.value);
   };
@@ -25,14 +31,18 @@ const GitSearch: React.FC = () => {
   const handleOnKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
       console.log('Search Term =>', query);
-      query.trim() && dispatch(gitSearch(query));
+      // query.trim() && dispatch(gitSearch(query));
+      query.trim() && dispatch(searchGitHubRepositories(query));
       setQuery('');
     }
   };
 
   const handleButtonOnClick = () => {
     console.log('Search Term =>', query);
-    query.trim() && dispatch(gitSearch(query));
+    // New Approach
+    // query.trim() && dispatch(gitSearch(query));
+    // Traditional Approach
+    query.trim() && dispatch(searchGitHubRepositories(query));
     setQuery('');
   };
 
