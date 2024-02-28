a8ce0131eb7c5af40cd17a8f487138911c9ef995
HamidHeyde <heidarzadeh.hamidreza@gmail.com>
2024-02-27 17:20:21 -0800

    updated the approach for dispatch and selector

--- portfolio/reactReduxBasics/src/traditionalApproach/redux/store.tsx
+++ portfolio/reactReduxBasics/src/traditionalApproach/redux/store.tsx
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import { gitSearchReducers } from './reducers';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 const rootReducers = combineReducers({
   gitSearch: gitSearchReducers
@@ -16,3 +17,5 @@ const store = createStore(
 export default store;
 
 export type RootState = ReturnType<typeof rootReducers>;
+export const useAppDispatch: () => typeof store.dispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
